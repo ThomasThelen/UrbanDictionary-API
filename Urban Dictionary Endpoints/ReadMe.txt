3b3ab35abfd5298e7b1b69fb49b9642efdc50c27
Thomas Thelen <tommythelen@gmail.com>
2018-02-04 22:55:34 -0800

    Upload Source & Remove Debug Info

---
+++ Urban Dictionary Endpoints/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : Urban Dictionary Endpoints Project Overview
+========================================================================
+
+AppWizard has created this Urban Dictionary Endpoints application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your Urban Dictionary Endpoints application.
+
+
+Urban Dictionary Endpoints.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+Urban Dictionary Endpoints.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+Urban Dictionary Endpoints.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named Urban Dictionary Endpoints.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ Urban Dictionary Endpoints/Urban Dictionary Endpoints.cpp
@@ -0,0 +1,133 @@
+#include "stdafx.h"
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous 
+
+
+int GetCommand()
+{
+	int command;
+	std::wcout << L"Communicate with Urban Dictionary\n";
+	std::wcout << L"Press '1' to define a word.\n";
+	std::wcout << L"Press '2' to get a random definition\n";
+	std::cin >> command;
+	return command;
+}
+
+
+pplx::task<web::http::http_response> CreateRequestTask(std::shared_ptr<ostream>& fileStream, std::wstring endPoint, std::wstring term=L"")
+{
+	// Open stream to output file.
+	return fstream::open_ostream(U("definition.html")).then([=](ostream outFile)
+	{
+		if (auto fileStream = std::make_shared<ostream>())
+		{
+			*fileStream = outFile;
+		}
+
+		// Start by creating an instance of the http_client object
+		http_client client(U("http://api.urbandictionary.com/v0/"));
+		// Define the end of the endpoint by using the builder object
+		auto queryUrl = std::wstring(endPoint).append(term);
+		auto builder = uri_builder(queryUrl);
+
+		// Create the request object, set the request method, and set the request uri.
+		auto request = http_request(methods::GET);
+		request.set_request_uri(builder.to_string());
+
+		// Send off the request
+		return  client.request(request);
+	});
+	//return requestTask;
+}
+
+void HandlePostRequest(Concurrency::task<web::http::http_response>& task, std::shared_ptr<ostream>& fileStream)
+{
+	task.then([=](http_response response)
+	{
+		// Write response body into the file.
+		return response.body().read_to_end(fileStream->streambuf());
+	})
+
+		// Close the file stream.
+		.then([=](size_t)
+	{
+		return fileStream->close();
+	});
+
+	// Wait for all the outstanding I/O to complete and handle any exceptions
+	try
+	{
+		task.wait();
+	}
+	catch (const std::exception &e)
+	{
+		printf("Error exception:%s\n", e.what());
+	}
+}
+
+void DefineWord()
+{
+	auto term = std::wstring();
+	std::wcout << L"Enter the search term:\n";
+	std::wcin >> term;
+
+	auto fileStream = std::make_shared<ostream>();
+	auto task = CreateRequestTask(fileStream, L"define?term=", term);
+	// Handle response headers arriving.
+
+	task.then([=](http_response response)
+	{
+		// Write response body into the file.
+		return response.body().read_to_end(fileStream->streambuf());
+	})
+
+		// Close the file stream.
+		.then([=](size_t)
+	{
+		return fileStream->close();
+	});
+
+	// Wait for all the outstanding I/O to complete and handle any exceptions
+	try
+	{
+		task.wait();
+	}
+	catch (const std::exception &e)
+	{
+		printf("Error exception:%s\n", e.what());
+	}
+
+	//HandlePostRequest(requestTask, fileStream);
+	
+}
+
+void RandomWord()
+{
+	auto fileStream = std::make_shared<ostream>();
+	auto requestTask = CreateRequestTask(fileStream, L"random");
+	HandlePostRequest(requestTask, fileStream);
+}
+
+int main(int argc, char* argv[])
+{
+	switch (GetCommand())
+	{
+	case 1:
+		DefineWord();
+		break;
+	case 2:
+		RandomWord();
+		break;
+	default:
+		std::cout << "Command not recognized\n";
+		GetCommand();
+		break;
+	}
+	return 0;
+}
